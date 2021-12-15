49d5bc650c460bfd8e26907df9ed3205813c6f6f
Yasen Kostov <qskatak04@gmail.com>
2021-12-15 20:43:00 +0200

    Initial Commit

--- lib/contact_bratac_web/controllers/page_controller.ex
+++ lib/contact_bratac_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule ContactBratacWeb.PageController do
   use ContactBratacWeb, :controller
 
   def index(conn, _params) do
+    put_flash(:info, "Welcome to Phoenix, from flash info!")
     render(conn, "index.html")
   end
 end
