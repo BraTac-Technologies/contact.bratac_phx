223681be25e1ce947e7a7fe1a3b9d1eae395ccb0
Yasen Kostov <qskatak04@gmail.com>
2021-12-28 15:44:27 +0200

    add /contacts resources

--- lib/contact_bratac_web/router.ex
+++ lib/contact_bratac_web/router.ex
@@ -18,6 +18,7 @@ defmodule ContactBratacWeb.Router do
     pipe_through :browser
 
     live "/", IndexLive
+    resources "/contacts", ContactController
   end
 
   # Other scopes may use custom stacks.
