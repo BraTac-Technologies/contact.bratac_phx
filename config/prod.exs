57b487b22712ff13de328e0152fa386a563083b3
Yasen Kostov <qskatak04@gmail.com>
2021-12-28 22:09:39 +0200

    change host to actual host url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :contact_bratac, ContactBratacWeb.Endpoint,
-  url: [host: "contact.bratac.net", port: 80],
+  url: [host: "http://167.172.163.148:4001", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
